a5f38aa498307b7dcbfdc96e9c43cee0d2e820c6
JuHyeong424 <wkdwngud963@gmail.com>
2026-01-06 00:33:39 +0900

    test:파일에 있는 상품 목록 출력 입력값에 N 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -105,7 +105,7 @@ describe("편의점", () => {
 
   test("파일에 있는 상품 목록 출력", async () => {
     await run({
-      inputs: ["[콜라-1]", "N", "N"],
+      inputs: ["[콜라-1]", "N", "N", "N"],
       expected: [
         /* prettier-ignore */
         "- 콜라 1,000원 10개 탄산2+1",
